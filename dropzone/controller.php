4fe0ec8ba2baf691d7c1bf4bf8f6552cf23a3811
HONGWEI LAI <arron@user.com.sg>
2016-10-12 00:18:12 +0800

    case study 1: dropzone.js
    use dropzone.js to complete upload file front end code. controller not
    complete, the idea is there

---
+++ dropzone/controller.php
@@ -0,0 +1,28 @@
+<?php
+
+class controller
+{
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     *	- or -
+     * 		http://example.com/index.php/welcome/index
+     *	- or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     *
+     * @see http://codeigniter.com/user_guide/general/urls.html
+     */
+    public function index() {
+    }
+
+    public function upload(){
+      return base64_encode(file_get_contents($_FILES['event_image']['tmp_name'][0]));
+    }
+
+}
